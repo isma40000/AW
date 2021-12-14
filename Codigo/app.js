3113378812fab9bd69e4084d2ab1409a2f4f87f0
Seryiii <62355786+Seryiii@users.noreply.github.com>
2021-12-14 17:54:53 +0100

    Update app.js

--- Codigo/app.js
+++ Codigo/app.js
@@ -1,53 +1,103 @@
-// app.js
-const config = require("./config");
-const DAOTasks = require("./DAOTasks");
-const utils = require("./utils");
-const path = require("path");
-const mysql = require("mysql");
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
+"use strict";
 
-// Crear un servidor Express.js
+//----------------------------TODOS LOS "require"s-------------------------//
+//LOS FAVICONS, MORGAN Y TAL SEGURAMENTE TENDRAMOS QUE CAMBIARLO
+const config = require("./config");
+const path = require("path");
+const express = require("express");
+const favicon = require("serve-favicon");
+const morgan = require("morgan");//
+const bodyParser = require("body-parser");
+const routerEmpleados = require("./routers/routerUsers");
+const routerClientes = require("./routers/routerQuestions");
+//const routerVehiculos = require("./routers/routerVehiculos");
+const session = require("express-session");
+const mysqlSession = require("express-mysql-session");
+
+//-----------------------------------APP------------------------------------//
+
 const app = express();
-// Crear un pool de conexiones a la base de datos de MySQL
-const pool = mysql.createPool(config.mysqlConfig);
-// Crear una instancia de DAOTasks
-const daoT = new DAOTasks(pool);
-app.use(express.static('public'));
-// Arrancar el servidor
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Recursos estaticos
+//LOS FAVICONS, MORGAN Y TAL SEGURAMENTE TENDRAMOS QUE CAMBIARLO
+const ficherosEstaticos = path.join(__dirname, "public");
+app.use(express.static(ficherosEstaticos));
+app.use(favicon(__dirname + "/public/img/favicon.png"));
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//----------------------------------SESSION---------------------------------//
+
+const MySQLStore = mysqlSession(session);
+const sessionStore = new MySQLStore(config.mysqlConfig);
+
+const middlewareSession = session({
+    saveUninitialized: true,
+    secret: "foobar34",
+    resave: false,
+    store: sessionStore
+});
+app.use(middlewareSession);
+
+app.use(function(request, response, next) {
+    console.log("request.session.currentUser ========> ", request.session.currentUser);
+    next();
+})
+
+//--------------------------------- RUTAS ----------------------------------//
+app.use("/users", routerUsers);
+app.use("/questions", routerQuestions);
+//app.use("/vehiculos", routerVehiculos);
+
+//----------------------------------LOGIN-----------------------------------//
+
+app.get("/", function (request, response) {
+    response.status(200);
+    response.redirect("/login");
+});
+
+app.get("/login", function (request, response) {
+    response.status(200);
+    response.render("login", { errorMsg: null });
+});
+
+app.get("/principal", function (request, response) {
+    response.status(200);
+    console.log(request.session.currentType);
+    response.render("principal",{msg: null, name: null, userType: request.session.currentType});
+});
+
+//----------------------------------LOGOUT-----------------------------------//
+
+app.get("/logout", function (request, response) {
+    response.status(200);
+    request.session.destroy();
+    response.render("login", { errorMsg: null });
+});
+
+
+//------------------------------ERRORMIDDLEWARE------------------------------//
+
+app.use(function(request, response, next) {
+    response.status(404);
+    response.render("error404", { url: request.url });
+});
+
+app.use(function(error, request, response, next) {
+    response.status(500);
+    response.render("error500", {mensaje: error.message, pila: error.stack });
+});
+
+//---------------------------------SERVIDOR---------------------------------//
+
 app.listen(config.port, function(err) {
- if (err) {
- console.log("ERROR al iniciar el servidor");
- }
- else {
- console.log(`Servidor arrancado en el puerto ${config.port}`);
- }
+   if (err) {
+       console.log("ERROR al iniciar el servidor");
+   }
+   else {
+       console.log(`Servidor arrancado en el puerto ${config.port}`);
+   }
 });
-app.get("/", function(request,response){
-    response.statusCode = 200;
-    response.setHeader("Content-Type","text/html");
-    response.write("Esta es la pagina raiz");
-    response.end();
-});
-app.get("/", function(request,response){
-    response.statusCode = 200;
-    response.type("text/plain; charset=utf-8");
-    response.end("Esta es la pagina raiz");
-});
-app.get("/users.html", function(request,response){
-    response.statusCode = 200;
-    response.type("text/plain; charset=utf-8");
-    response.end("Aquí se mostrará la página de usuarios");
-});
-app.get("/usuarios.html", function(request,response){
-    response.redirect("/users.html");
-});
-app.listen(config.port, function(err) {
-    if (err) {
-    console.log("ERROR al iniciar el servidor: " + err.message);
-    }
-    else {
-    console.log(`Servidor arrancado en el puerto ${config.port}`);
-    }
-   });
